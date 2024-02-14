5736d3d34aec4a100b0753b43a53b5d9345abb2d
Superredstone <patrickcanal3@gmail.com>
2024-02-14 11:27:55 +0100

    Improved lock screen

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -2,5 +2,6 @@
 
 set -e 
 xset s off dpms 0 10 0
-i3lock --ignore-empty-password --show-failed-attempts --nofork
+# i3lock --ignore-empty-password --show-failed-attempts --nofork
+i3lock --ignore-empty-password --show-failed-attempts --blur 5 --clock --time-str=%H:%M --indicator --inside-color="#FFFFFF11"
 xset s off -dpms 
