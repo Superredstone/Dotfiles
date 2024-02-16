e4cf80e496a612daeed6927320d449fc365de0d6
Superredstone <patrickcanal3@gmail.com>
2024-02-16 16:54:18 +0100

    Added controller battery on Polybar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting 
+    set TERM screen-256color 
     pokemon-colorscripts -r --no-title
 end
 
+zoxide init --cmd cd fish | source
